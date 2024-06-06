4176afe6f92d5c351eae6b3d9374e254572cdbe6
Kevin Gilbert <kevin@heartsmartkids.com>
2024-06-06 15:56:02 -0600

    playing around llm with streamlit

---
+++ llmchat/llmchat.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+st.title("ChatGPT-like clone")
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set OpenAI API key from environment
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
