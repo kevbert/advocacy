b0514c5f02ec28e636a293f92f2d6299475968ba
Kevin Gilbert <kevin@heartsmartkids.com>
2024-06-13 11:28:11 -0600

    initial Assitant connection for intro text

--- advocacy/Home.py
+++ advocacy/Home.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from openai import OpenAI
+import threading
+import time
+
+@st.cache_data   #TODO check if this caching is working
+def run_thread(thread_id, assistant_id, _client):
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+        instructions="Answer user questions based on the reference materials."
+    )
+    return run
 
 st.set_page_config(
     page_title="Advocacy",
@@ -10,7 +22,48 @@ st.title("Advocacy Home")
 # add logo file "TextBotLogoSmall.jpeg" to sidebar
 st.logo("TextBotLogoSmall.jpeg")
 
-st.sidebar.success("Reference Materials Loaded")
+# get OPENAI_API_KEY from .env file
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(".", ".env"))
+
+#set up OpenAI API stuff
+# Access the API key from the environment
+api_key = os.getenv("ADVOCACY_OPENAI_API_KEY")
+# connect to LLM Assistant API
+client = OpenAI(api_key=api_key)
+#fetch assistant
+assistant = client.beta.assistants.retrieve("asst_NKfcLtfzuj3uCPa1cDlNNlTy")
+#create a thread for this session
+thread = client.beta.threads.create()
+
+#start with a simple message to get basic info and assure connection
+#initial info message on thread
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content="What is the agency, title, and due date for comments on this call? Example response: '''CMS, CY 2023 Physician Fee Schedule, comments due September 30, 2022.'''" 
+)
+# run thread on assistant
+run = run_thread(thread.id, assistant.id, client)
+# Poll for task completion
+while run.status != "completed":
+    #show status in sidebar
+    st.sidebar.info(f"Task status: {run.status}")
+    time.sleep(1)  # Adjust polling interval as needed
+    #TODO: should check timeout and errors
+
+st.sidebar.success("Assistant Connected")
+
+messages = client.beta.threads.messages.list(thread_id=thread.id)
+#print content all messages in main window
+for message in messages.data:
+    st.write(f"{message.role} ({message.id}): {message.content[0].text.value}")
+
 
 ai_url = "https://openai.com"
 current_document = "CMS Document Long <NONE>"
